5cc83cf05a1281d38a5b48b0c83287bfb6941a8a
Hieu Doan <hieudoan280102@gmail.com>
2024-01-24 16:44:15 +0700

    test get list packages

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const PORT = 5000;
+// app.use(cors());
+// const db = require('./database/dtb');
+// const User = require('./database/Schemas/User');
+// const Booking = require('./database/Schemas/Booking');
+// const Packages = require('./routes/packagesRoutes.js');
+
+// app.use('/api', Packages); 
+// db();
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 5000;
+
 app.use(cors());
+
 const db = require('./database/dtb');
 const User = require('./database/Schemas/User');
 const Booking = require('./database/Schemas/Booking');
+const Packages = require('./routes/packagesRoutes.js');
+
+// Use the packageRouter for requests to /api/packages
+app.use('/api/packages', Packages);
+
 db();
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
