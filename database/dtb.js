a2d421b8e2b281966f057afad3bac64e340b75da
MinhHuy-SE170417 <huytran21a@gmail.com>
2024-01-24 12:22:53 +0700

    created db
    created db

--- database/dtb.js
+++ database/dtb.js
@@ -1,14 +1,16 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-export default async function connectDB() {
-  const url = "mongodb://localhost:27017/booking-birthday";
+async function connectDB() {
+    const url = 'mongodb://localhost:27017/booking-birthday';
 
-  try {
-    await mongoose.connect(url).then((data) => {
-      console.log(`Database connect with ${data.connection.host}`);
-    });
-  } catch (err) {
-    console.error(err.message);
-    setTimeout(connectDB, 5000);
-  }
+    try {
+        await mongoose.connect(url).then((data) => {
+            console.log(`Database connect with ${data.connection.host}`);
+        });
+    } catch (err) {
+        console.error(err.message);
+        setTimeout(connectDB, 5000);
+    }
 }
+
+module.exports = connectDB;
