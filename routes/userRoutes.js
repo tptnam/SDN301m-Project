0f1ff511acab65d3a53f412085a2ffffaf4b6211
MinhHuy-SE170417 <huytran21a@gmail.com>
2024-01-25 10:11:51 +0700

    updated route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 const userControllers = require('../controllers/userControllers');
-const updateUserControllers = require('../controllers/updateControllers');
 router.get('/users', userControllers.getUsers);
 router.get('/users/:id', userControllers.getUserById);
 router.put('/users/delete', userControllers.deleteUser);
