34257d304d1da50a3d5222705286f0b41a4d9ded
MinhHuy-SE170417 <huytran21a@gmail.com>
2024-01-25 10:10:32 +0700

    merged LinhDTT

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,30 @@
 const User = require('../database/Schemas/User');
 const { validatePassword, comparePassword } = require('../utils/helpers');
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.sendStatus(400);
+    }
+    try {
+        const userDB = await User.findOne({ email });
+        if (!userDB)
+            return res
+                .status(401)
+                .send({ error: 'Incorrect email or password' });
+        const isValid = comparePassword(password, userDB.password);
+        if (isValid) {
+            return res.sendStatus(200);
+        } else
+            return res
+                .status(401)
+                .send({ error: 'Incorrect email or password' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ error: 'An error occurred' });
+    }
+};
+
 const registerAccount = async (req, res) => {
     const { password, email } = req.body;
     try {
@@ -62,4 +86,4 @@ const changePassword = async (req, res) => {
     }
 };
 
-module.exports = { changePassword, registerAccount };
+module.exports = { changePassword, registerAccount, login };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,5 @@ const userControllers = require('../controllers/userControllers');
 const updateUserControllers = require('../controllers/updateControllers');
 router.get('/users', userControllers.getUsers);
 router.get('/users/:id', userControllers.getUserById);
-router.put('/users/update', updateUserControllers.updateUser);
 router.put('/users/delete', userControllers.deleteUser);
 module.exports = router;
