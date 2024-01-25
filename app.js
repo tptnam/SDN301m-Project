c6dcb332b217ed06e4764c4c26b90a8ad5a902c2
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-01-25 10:02:01 +0700

    clean up my branch

--- app.js
+++ app.js
@@ -10,13 +10,10 @@ app.use(bodyParser.json());
 const db = require("./database/dtb");
 const User = require("./database/Schemas/User");
 const Booking = require("./database/Schemas/Booking");
-const Packages = require("./routes/packagesRoutes.js");
-const StyleParty = require("./routes/stylePartyRoutes.js");
+
 const menuRouter = require("./routes/MenuRoutes.js");
 const ServiceRouter = require("./routes/ServiceRoutes.js");
 
-app.use("/api", Packages);
-app.use("/api", StyleParty);
 app.use("/api", menuRouter);
 app.use("/api", ServiceRouter);
 
