a2d5345577d9fa05b566372eb476ebdb8927f260
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-02-21 14:56:32 +0700

    m

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = 5000;
 app.use(cors());
-const db = require('./database/dtb');
-const User = require('./database/Schemas/User');
-const Booking = require('./database/Schemas/Booking');
-const authRoutes = require('./routes/authRoutes');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
+const db = require("./database/dtb");
+const User = require("./database/Schemas/User");
+const Booking = require("./database/Schemas/Booking");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+
 db();
+
 app.use(bodyParser());
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.all('/', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1', userRoutes);
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1", userRoutes);
