c6d01d4ea718287b6ef515a1e9f72a0e6551709f
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-03-07 07:51:19 +0700

    update...

--- routes/MenuRoutes.js
+++ routes/MenuRoutes.js
@@ -14,7 +14,7 @@ const menuRouter = express.Router();
 menuRouter.get("/menu", getAllMenu);
 menuRouter.get("/menu/:id", getMenuById);
 menuRouter.post("/menu/", createMenu);
-menuRouter.put("/menu/:id", verifyToken, updateMenu);
-menuRouter.delete("/menu/:id", verifyToken, deleteMenu);
+menuRouter.put("/menu/:id", updateMenu);
+menuRouter.delete("/menu/:id", deleteMenu);
 
 module.exports = menuRouter;
